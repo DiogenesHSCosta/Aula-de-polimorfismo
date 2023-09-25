02e7f908406254e1e22401e42e0984ab12fbb85f
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-09-25 17:56:59 -0300

    segundo commit

---
+++ costaBank/src/costaBank/TesteCriar.java
@@ -0,0 +1,16 @@
+package costaBank;
+
+public class TesteCriar {
+
+	public static void main(String[] args) {
+		String nome = "José Manoel";
+		String cpf = "552.552.552-52";
+		double salario= 1000.00;
+		
+		
+		Funcionario teste = new Funcionario(nome, cpf, salario);
+		System.out.println(teste.getNome());
+		System.out.println(teste.getSalario());
+		System.out.println(teste.getBonificacao());
+	}
+}
