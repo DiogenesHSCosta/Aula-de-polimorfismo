bb092c79143a54404ff0c7188c342c1fa6b862c5
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-09-25 19:15:28 -0300

    implementação da classe gerente que herda funcionario

---
+++ costaBank/src/costaBank/Gerente.java
@@ -0,0 +1,23 @@
+package costaBank;
+
+public class Gerente extends Funcionario {
+	private String senha;
+	
+	public boolean getAutenticacao(String senha) {
+		
+		if(this.senha == senha) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public double getBonificacao() {
+		return salario;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
